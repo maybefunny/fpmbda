2704c26d5120a774b14f506f4c9471bf600b72aa
sulton <darfisultoni@gmail.com>
2019-05-21 20:12:15 +0700

    view done

--- FP_views.sql
+++ FP_views.sql
@@ -14,7 +14,48 @@
         komunitas.kmt_nama = 'TCyber';
 
 -- Daftar jumlah modul yang bisa dikerjakan oleh setiap anggota komunitas (usr_id | count(md) )
--- Daftar Jumlah modul yang sudah dikerjakan oleh setiap anggota komunitas (md_id | coutn(md )
+    CREATE OR REPLACE VIEW "JML_MODUL_BLM_SELESAI_TCYBER" ("USR_NAMA", "JML") AS 
+    SELECT
+        users.usr_nama,
+        (
+          select
+            count(md_id)
+          from
+            modul
+          where
+            modul.kmt_id = komunitas.kmt_id
+        )-(
+          select
+            count(status_selesai) as jml
+          from
+            user_modul
+          where
+            user_modul.usr_id=users.usr_id
+            and status_selesai=1
+        ) as jml
+    FROM
+        users
+        join user_komunitas on user_komunitas.usr_id = users.usr_id
+        join komunitas on user_komunitas.kmt_id = komunitas.kmt_id
+    WHERE
+        komunitas.kmt_nama = 'TCyber';
+
+-- Daftar Jumlah modul yang sudah dikerjakan oleh setiap anggota komunitas (usr_id | count(md )
+    CREATE OR REPLACE VIEW Jumlah_Modul_Selesai_Tcyber AS
+    SELECT
+        users.usr_nama,
+        (select
+          count(status_selesai) as jml
+        from
+          user_modul
+        where
+          user_modul.usr_id=users.usr_id) as jml
+    FROM
+        users
+        join user_komunitas on user_komunitas.usr_id = users.usr_id
+        join komunitas on user_komunitas.kmt_id = komunitas.kmt_id
+    WHERE
+        komunitas.kmt_nama = 'TCyber'
 -- Daftar user dari suatu komunitas (usr_id | usr_nama)
     CREATE OR REPLACE VIEW Anggota_TCyber AS
     SELECT
@@ -36,3 +77,11 @@
     GROUP BY
         komunitas.kmt_nama;
 -- Jumlah modul dari tiap-tiap komunitas (kmt_id | count(md))
+    CREATE OR REPLACE VIEW Jumlah_Modul_Per_Komunitas AS
+    SELECT
+        komunitas.kmt_nama AS komunitas, count(modul.md_id) AS jumlah_modul
+    FROM
+        modul
+        join komunitas on komunitas.kmt_id = modul.kmt_id
+    GROUP BY
+        komunitas.kmt_nama;
