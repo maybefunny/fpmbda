882646a11073992b629f49fac727161587e354e4
Setya Wibawa <durianpeople@gmail.com>
2019-05-21 20:34:21 +0700

    Update FP_functions.sql

--- FP_functions.sql
+++ FP_functions.sql
@@ -1,14 +1,40 @@
--- Cek jika user bisa mengikuti suatu modul
+-- Cek jika user bisa mengikuti suatu modul berdasarkan syarat
 
-CREATE OR REPLACE FUNCTION bisa_ikut_modul(usr_id_in IN char, md_id_in IN char)
+CREATE OR REPLACE FUNCTION boleh_ikut_modul(usr_id_in IN char, md_id_in IN char)
 RETURN SMALLINT
-IS boleh INT;
+IS 
+    req INT;
+    rel INT;
 BEGIN
-    SELECT CT INTO boleh FROM (
-        SELECT STATUS_SELESAI, COUNT(STATUS_SELESAI) AS CT FROM USER_MODUL
-        WHERE USR_ID = 'USR0000001' GROUP BY STATUS_SELESAI HAVING STATUS_SELESAI = '0'
+    SELECT COUNT(MD_ID) INTO req FROM SYARAT
+    WHERE MD_ID = md_id_in;
+
+    SELECT COUNT(MD_ID) INTO rel FROM USER_MODUL
+    WHERE USR_ID = usr_id_in AND MD_ID IN (
+        SELECT MOD_MD_ID FROM SYARAT
+        WHERE MD_ID = md_id_in
     );
-    IF boleh > 0 THEN RETURN 0;
-    ELSE RETURN 1;
+
+    IF req = rel THEN RETURN 1;
+    ELSE RETURN 0;
     END IF;
-END bisa_ikut_modul;
+END;
+
+-- Cek jika user bisa mengikuti suatu modul berdasarkan keanggotaan
+
+CREATE OR REPLACE FUNCTION bisa_ikut_modul(usr id in IN CHAR, md_id_in IN CHAR)
+RETURN SMALLINT
+IS 
+    boleh INT;
+    md_kmt_id CHAR(10);
+    usr_kmt_id CHAR(10);
+BEGIN
+    SELECT KMT_ID INTO md_kmt_id FROM MODUL
+    WHERE MD_ID = md_id_in;
+    SELECT KMT_ID INTO usr_kmt_id FROM USER_MODUL
+    WHERE usr_modul = usr_modul_in;
+    IF md_kmt_id = usr_kmt_id THEN
+    RETURN 1;
+    ELSE RETURN 0;
+    END IF;
+END;
