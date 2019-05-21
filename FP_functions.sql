9ea3af3fb1fea3ad8f142ad27c3d676820a6e61e
sulton <darfisultoni@gmail.com>
2019-05-21 20:24:02 +0700

    is_admin

--- FP_functions.sql
+++ FP_functions.sql
@@ -6,9 +6,24 @@ IS boleh INT;
 BEGIN
     SELECT CT INTO boleh FROM (
         SELECT STATUS_SELESAI, COUNT(STATUS_SELESAI) AS CT FROM USER_MODUL
-        WHERE USR_ID = 'USR0000001' GROUP BY STATUS_SELESAI HAVING STATUS_SELESAI = '0'
+        WHERE USR_ID = usr_id_in GROUP BY STATUS_SELESAI HAVING STATUS_SELESAI = '0'
     );
     IF boleh > 0 THEN RETURN 0;
     ELSE RETURN 1;
     END IF;
 END bisa_ikut_modul;
+
+-- Cek jika user merupakan admin dari sebuah komunitas (argumen: USR_ID, KMT_ID) (return: true/false)
+
+CREATE OR REPLACE FUNCTION is_admin(usr_id_in IN char, kmt_id_in IN char)
+RETURN SMALLINT
+IS administrator INT;
+BEGIN
+    SELECT STATUS INTO administrator FROM (
+        SELECT STATUS FROM USER_KOMUNITAS
+        WHERE USR_ID = usr_id_in AND KMT_ID = kmt_id_in
+    );
+    IF administrator = 2 THEN RETURN 1;
+    ELSE RETURN 0;
+    END IF;
+END is_admin;
